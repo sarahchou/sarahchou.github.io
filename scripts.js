3f3f3b369019c23d63cb68dea974a9e1d12878a2
sarahchou <chou.s@husky.neu.edu>
2020-07-16 22:14:27 -0700

    adding scrolling capabilities in javascript

---
+++ scripts.js
@@ -0,0 +1,14 @@
+$(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      var target = $(this.hash);
+      if (target.length) {
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top
+          },
+          1000
+        );
+      }
+    });
+  });
+  
